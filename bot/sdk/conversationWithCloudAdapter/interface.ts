57be5dc4e6058e482049ec7b1508ac15ad19afba
Qinen <qinzhu@microsoft.com>
2023-04-06 14:29:32 +0800

    Add new interface ConversationReferenceStore for customization

--- bot/sdk/conversationWithCloudAdapter/interface.ts
+++ bot/sdk/conversationWithCloudAdapter/interface.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { CloudAdapter } from "botbuilder";
+import { CloudAdapter, ConversationReference } from "botbuilder";
 import {
   NotificationTargetStorage,
   BotSsoConfig,
@@ -21,8 +21,21 @@ export interface NotificationOptions {
    */
   botAppId?: string;
   /**
+   * An optional store to persist bot notification connections.
+   * 
+   * @remarks
+   * If `store` is not provided, a default conversation reference store will be used,
+   * which stores notification connections into:
+   *  - `.notification.localstore.json` if running locally
+   *  - `${process.env.TEMP}/.notification.localstore.json` if `process.env.RUNNING_ON_AZURE` is set to "1"
+   * 
+   * It's recommended to use your own store for production environment.
+   */
+  store?: ConversationReferenceStore;
+  /**
    * An optional storage to persist bot notification connections.
    *
+   * @deprecated Use `store` to customize the way to persist bot notification connections instead.
    * @remarks
    * If `storage` is not provided, a default local file storage will be used,
    * which stores notification connections into:
@@ -90,4 +103,43 @@ export interface ConversationOptions {
      */
     enabled?: boolean;
   };
+}
+
+export interface ConversationReferenceStore {
+  /**
+   * Add a conversation reference to the store. If overwrite, update existing one, otherwise add when not exist.
+   * @returns true if added or updated, false if not changed.
+   */
+  add(
+    key: string,
+    reference: Partial<ConversationReference>,
+    options?: ConversationReferenceStoreAddOptions
+  ): Promise<boolean>;
+
+  /**
+   * Remove a conversation reference from the store.
+   * @returns true if exist and removed, false if not changed.
+   */
+  remove(
+    key: string,
+    reference: Partial<ConversationReference>
+  ): Promise<boolean>;
+
+  /**
+   * List stored conversation reference by page.
+   * @returns a paged list.
+   */
+  list(
+    pageSize?: number,
+    continuationToken?: string
+  ): Promise<PagedData<Partial<ConversationReference>[]>>;
+}
+
+export interface ConversationReferenceStoreAddOptions {
+  overwrite?: boolean;
+}
+
+export interface PagedData<T> {
+  data: T;
+  continuationToken?: string;
 }
